9090565482351ca6c2bbbf99be76df94cdd7496c
Eduardo <edu.denegri@gmail.com>
2025-12-27 13:31:40 -0300

    minor change

--- Clon-X-frontend/src/pages/Profile.jsx
+++ Clon-X-frontend/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { API_URL } from "../config";
 import EditProfileModal from "../components/EditProfileModal";
-import { UserContext } from "../context/userContext";
+import { UserContext } from "../context/UserContext";
 import { Calendar } from "lucide-react";
 import { PostCard } from "./Home";
 import { getPostsByUserId, getPostsLikedByUserId } from "../services/postService";
